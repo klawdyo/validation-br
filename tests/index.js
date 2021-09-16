b3246f02ab8ee2dd024611be9e9a6d63d7d4a66d
Cláudio Medeiros <klawdyo@gmail.com>
2021-09-16 18:52:27 -0300

    test: Ativação dos testes

--- tests/index.js
+++ tests/index.js
@@ -1,12 +1,12 @@
 // Funções de Validação
-// require('./is-cpf.test');
-// require('./is-cnpj.test');
-// require('./is-titulo.test');
-// require('./is-postal-code.test');
+require('./is-cpf.test');
+require('./is-cnpj.test');
+require('./is-titulo.test');
+require('./is-postal-code.test');
 require('./is-cnh.test');
 // require('./is-renavam.test');
 
 // Funções Auxiliares
-// require('./sum-to-dv.test');
-// require('./invalid-list-generator.test');
-// require('./sum-elements-by-multipliers.test');
+require('./sum-to-dv.test');
+require('./invalid-list-generator.test');
+require('./sum-elements-by-multipliers.test');
