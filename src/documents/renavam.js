d85dec173b7a9cff9f5bd04acfeb0093cc34e9ab
Cláudio Medeiros <klawdyo@gmail.com>
2022-01-08 18:46:51 -0300

    docs: Inclusão de comentários no código de renavam

--- src/documents/renavam.js
+++ src/documents/renavam.js
@@ -1,3 +1,39 @@
+/**
+ * RENAVAM
+ * Funções auxiliares para cálculo de máscaras, validação, dígito verificador e criaçãode
+ * números fake.
+ *
+ * @doc
+ * - O número de RENAVAM deve possuir 11 caracteres
+ *
+ * - Os caracteres de 1 a 10 são a numeração documento
+ *
+ * - O caractere 11 é o dígito verificador.
+ *
+ * 1) Partes do número
+ *  _______________________________________________
+ * |  Número                                 | D V |
+ * |  2   6   8   2   7   6   4   9   9   6  -  0  |
+ * |_________________________________________|_____|
+ *
+ * 2) Cálculo do DV.
+ *
+ *  - Soma-se o produto das algarismos 3 a 10 pelos números 3, 2, 9, 8, 7, 6, 5, 4, 3, 2
+ *
+ *    2   6   8   2   7   6   4   9   9   6
+ *    x   x   x   x   x   x   x   x   x   x
+ *    3   2   9   8   7   6   5   4   3   2
+ * =  6 +12 +72 +16 +49 +12 +20 +36 +27 +12  =  234
+ *
+ *  - O somatório encontrado é multiplicado por 10 e ao resultado
+ *    é aplicado o cálculo do MOD 11.
+ *
+ *    ( 234 * 10 ) / 11 tem resto 8. DV = 8. Caso o resto seja maior ou igual a
+ *    10, DV será 0.
+ *
+ *
+ */
+
 const {
   sumElementsByMultipliers, fakeNumber, applyMask, clearValue,
 } = require('../lib/utils');
