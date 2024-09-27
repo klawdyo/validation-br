70eba1dcdf43c64895121bb43a49d88e99d3ab8b
Claudio Medeiros <klawdyo@gmail.com>
2024-09-27 09:02:51 -0300

    fix: main recebe função padrão de validação

---
+++ src/main.ts
@@ -0,0 +1,49 @@
+import { validate as cnh } from './cnh';
+import { validate as cnpj } from './cnpj';
+import { validate as cpf } from './cpf';
+import { validate as judicialProcess } from './judicialProcess';
+import { validate as nup17 } from './nup17';
+import { validate as pisPasep } from './pisPasep';
+import { validate as postalCode } from './postalCode';
+import { validate as renavam } from './renavam';
+import { validate as tituloEleitor } from './tituloEleitor';
+import { CarPlate } from './carplate';
+
+
+function validate(fn: Function) {
+  try {
+    fn()
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
+
+
+export const isCNH = (value: string | number): boolean => cnh(value);
+export const isCNPJ = (value: string | number): boolean => cnpj(value);
+export const isCPF = (value: string | number): boolean => cpf(value);
+export const isJudicialProcess = (value: string): boolean => judicialProcess(value);
+export const isPIS = (value: string): boolean => pisPasep(value);
+export const isPostalCode = (value: string): boolean => postalCode(value);
+export const isRenavam = (value: string): boolean => renavam(value);
+export const isTituloEleitor = (value: string | number): boolean => tituloEleitor(value);
+export const isNUP17 = (value: string): boolean => nup17(value);
+
+
+// export const isCarPlate = (value: string) => carPlate(value);
+export function isCarPlate(value: string) { return validate(() => new CarPlate(value)) }
+
+export default {
+  isCNH,
+  isCNPJ,
+  isCPF,
+  isJudicialProcess,
+  isPIS,
+  isPostalCode,
+  isRenavam,
+  isTituloEleitor,
+  isNUP17,
+  isCarPlate
+}
