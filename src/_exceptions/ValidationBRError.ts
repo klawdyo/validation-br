7aea3680a6e443adc866fd568181c4d0e8a73eab
Claudio Medeiros <klawdyo@gmail.com>
2024-09-27 08:51:54 -0300

    fix: Inclui erro para funções sem dígito verificador

--- src/_exceptions/ValidationBRError.ts
+++ src/_exceptions/ValidationBRError.ts
@@ -4,4 +4,5 @@ export default class ValidationBRError extends Error {
   static EMPTY_VALUE = new ValidationBRError('Valor não preenchido')
   static MAX_LEN_EXCEDEED = new ValidationBRError('Número de caracteres excedido')
   static REPEATED_SEQUENCE = new ValidationBRError('Sequência de números repetidos não permitida')
+  static NO_CHECKSUM = new ValidationBRError('Este objeto não possui dígito verificador')
 }
