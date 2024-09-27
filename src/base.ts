8bd63ca2bc7c9ea1fe4c9d0f2243de245e31927e
Claudio Medeiros <klawdyo@gmail.com>
2024-09-27 08:51:37 -0300

    fix: interface substituída por classe abstrata

---
+++ src/base.ts
@@ -0,0 +1,70 @@
+import { Mask } from "./_helpers/mask"
+
+export abstract class Base {
+  /**
+   * Estilo da máscara no formato: 000.000.000-00
+   * 
+   */
+  protected abstract _mask: string;
+
+  constructor(protected _value: string, fakeFn?: Function) { }
+
+  get value() {
+    return this._value
+  }
+
+  toString() {
+    return this.value
+  }
+
+  // 
+  // 
+  // 
+  // 
+  // 
+
+  /**
+   * Deve remover apenas pontos e traços.
+   */
+  protected abstract normalize(value: string): void;
+
+  /**
+   * Deve aplicar as regras de validação do dígito verificador do documento
+   */
+  protected abstract validate(value: string): boolean;
+
+
+  // 
+  // 
+  // 
+  // 
+  // 
+
+  /**
+   * Deve devolver o número com a máscara
+   */
+  mask(options?: any): string {
+    return new Mask(this._value).apply(this._mask)
+  }
+
+  // 
+  // 
+  // STATIC
+  // 
+  // 
+
+  /**
+   * Deve calcular o dígito verificador do número passado sem o dígito
+   */
+  static checksum(value: string): string | null {
+    throw new Error('Should implement on child')
+  }
+
+  /**
+   * Deve gerar um número fake no padrão
+   */
+  static fake(options?: any): any {
+    throw new Error('Should implement on child')
+    // Base.fakeFn(options)
+  }
+}
