1fc17ee1180ea68b8b081053993c80ea249a3636
Claudio Medeiros <klawdyo@gmail.com>
2024-09-30 14:37:38 -0300

    fix: migra cnh pra object value

--- src/cnh.ts
+++ src/cnh.ts
@@ -54,87 +54,68 @@
  * @returns {Boolean}
  */
 
-import  { InvalidChecksumException } from './_exceptions/ValidationBRError'
-import { sumElementsByMultipliers, sumToDV, clearValue, applyMask, fakeNumber } from './utils'
+import { InvalidChecksumException } from './_exceptions/ValidationBRError';
+import { Base } from './base';
+import { sumElementsByMultipliers, sumToDV, clearValue, applyMask, fakeNumber } from './utils';
 
-/**
- * Calcula o Dígito Verificador de um RENAVAM informado
- *
- * @returns String Número fake de um cnh válido
- */
-export const dv = (value: string | number): string => {
-  const cnh = clearValue(value, 9, {
-    trimAtRight: true,
-    rejectEmpty: true,
-  })
+export class CNH extends Base {
+  protected _mask = '000000000-00';
 
-  const sum1 = sumElementsByMultipliers(cnh.substring(0, 9), [2, 3, 4, 5, 6, 7, 8, 9, 10])
-  const dv1 = sumToDV(sum1)
-
-  const sum2 = sumElementsByMultipliers(cnh.substring(0, 9) + dv1, [3, 4, 5, 6, 7, 8, 9, 10, 11, 2])
-  const dv2 = sumToDV(sum2)
-
-  return `${dv1}${dv2}`
-}
-
-/**
- * validateOrFail()
- * Valida se um número é válido e
- * retorna uma exceção se não estiver
- *
- * @param {String|Number} value Número a ser validado
- * @returns {Boolean}
- */
-export const validateOrFail = (value: string | number): boolean => {
-  const cnh = clearValue(value, 11, {
-    fillZerosAtLeft: true,
-    rejectEmpty: true,
-    rejectHigherLength: true,
-    rejectEqualSequence: true,
-  })
-
-  if (dv(cnh) !== cnh.substring(9, 11)) {
-    throw new InvalidChecksumException()
+  constructor(protected _value: string) {
+    super(_value);
+    this.normalize();
   }
 
-  return true
-}
+  protected normalize(): void {
+    this._value = this._value.replace(/^[.-]$/, '');
+  }
 
-/**
- * validate()
- * Valida se um número é válido
- *
- * @param {String|Number} value Número a ser validado
- * @returns {Boolean}
- */
-export const validate = (value: string | number): boolean => {
-  try {
-    return validateOrFail(value)
-  } catch (error) {
-    return false
+  /**
+   * Calcula o Dígito Verificador de um RENAVAM informado
+   *
+   * @returns String Número fake de um cnh válido
+   */
+  static checksum(value: string | number): string {
+    const cnh = clearValue(value, 9, {
+      trimAtRight: true,
+      rejectEmpty: true,
+    });
+
+    const sum1 = sumElementsByMultipliers(cnh.substring(0, 9), [2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    const dv1 = sumToDV(sum1);
+
+    const sum2 = sumElementsByMultipliers(cnh.substring(0, 9) + dv1, [3, 4, 5, 6, 7, 8, 9, 10, 11, 2]);
+    const dv2 = sumToDV(sum2);
+
+    return `${dv1}${dv2}`;
+  }
+
+  /**
+   * validateOrFail()
+   * Valida se um número é válido e
+   * retorna uma exceção se não estiver
+   *
+   * @param {String|Number} value Número a ser validado
+   * @returns {Boolean}
+   */
+  validate(value: string | number): boolean {
+    const cnh = clearValue(value, 11, {
+      fillZerosAtLeft: true,
+      rejectEmpty: true,
+      rejectHigherLength: true,
+      rejectEqualSequence: true,
+    });
+
+    return CNH.checksum(cnh) !== cnh.substring(9, 11);
+  }
+
+  /**
+   * Cria um número fake
+   *
+   * @returns String Número fake porém válido
+   */
+  static fake(): CNH {
+    const value = fakeNumber(9, true);
+    return new CNH(`${value}${CNH.checksum(value)}`);
   }
 }
-
-/**
- * Aplica uma máscara a uma string
- *
- * @returns String string com a máscara aplicada
- */
-export const mask = (value: string | number): string => applyMask(value, '000000000-00')
-
-/**
- * Cria um número fake
- *
- * @returns String Número fake porém válido
- */
-export const fake = (withMask: boolean = false): string => {
-  const value = fakeNumber(9, true)
-
-  const cnh = `${value}${dv(value)}`
-
-  if (withMask) return mask(cnh)
-
-  return cnh
-}
-
-export default validate
---
+++ test/cnh.spec.ts
@@ -0,0 +1,58 @@
+import { CNH } from '../src/cnh';
+import * as _cnh from '../src/cnh';
+
+describe('CNH', () => {
+  describe('constructor', () => {
+    test.each([
+      '50195131143',
+      '58316794534',
+      '50195471165',
+      '69044271146',
+      '46190906839',
+      '624729276-37',
+    ])('deve estar definido', (input) => {
+      expect(new CNH(input)).toBeDefined();
+    });
+
+    test.each([
+      '50195131143',
+      '58316794534',
+      '50195471165',
+      '69044271146',
+      '46190906839',
+      '624729276-37',
+    ])('deve lançar erro', (input) => {
+      expect(new CNH(input)).toBeDefined();
+    });
+  });
+
+  describe('fake', () => {
+    test.each([...Array(5)])('Gera fakes', () => {
+      const cnh = CNH.fake();
+      expect(cnh).toBeDefined();
+      expect(cnh.value).toHaveLength(11);
+    });
+  });
+
+  test.each([
+    { num: '501954711', expected: '65' },
+    { num: '583164745', expected: '75' },
+    { num: '690444711', expected: '17' },
+  ])('checksum() - Verificando se o DV gerado está correto', (item) => {
+    const calcDv = CNH.checksum(item.num);
+
+    expect(calcDv).toBe(item.expected);
+    expect(typeof calcDv).toBe('string');
+  });
+
+  test.each([
+    { value: '50195471143', expected: '501954711-43' },
+    { value: '58316474534', expected: '583164745-34' },
+    { value: '69044471146', expected: '690444711-46' },
+  ])('mask() - Testando se a máscara foi gerada corretamente', (input) => {
+    const cnh = new CNH(input.value);
+
+    expect(cnh.mask()).toBe(input.expected);
+    expect(cnh.mask()).toHaveLength(12);
+  });
+});
