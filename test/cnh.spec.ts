225713ad8d193c4e5c117f48b6f3bd119eb58a40
Claudio Medeiros <klawdyo@gmail.com>
2024-10-02 11:56:26 -0300

    test: corerção dos testes de cnh

--- test/cnh.spec.ts
+++ test/cnh.spec.ts
@@ -3,26 +3,22 @@ import * as _cnh from '../src/cnh';
 
 describe('CNH', () => {
   describe('constructor', () => {
-    test.each([
-      '50195131143',
-      '58316794534',
-      '50195471165',
-      '69044271146',
-      '46190906839',
-      '624729276-37',
-    ])('deve estar definido', (input) => {
-      expect(new CNH(input)).toBeDefined();
-    });
+    test.each(['14490435923', '97286888262', '621049358-60', '491872350-50'])(
+      'deve estar definido',
+      (input) => {
+        expect(new CNH(input)).toBeDefined();
+      }
+    );
 
     test.each([
-      '50195131143',
-      '58316794534',
-      '50195471165',
-      '69044271146',
-      '46190906839',
-      '624729276-37',
+      '50995131143',
+      '58916794534',
+      '50995471165',
+      '69944271146',
+      '46990906839',
+      '629729276-37',
     ])('deve lançar erro', (input) => {
-      expect(new CNH(input)).toBeDefined();
+      expect(() => new CNH(input)).toThrow();
     });
   });
 
@@ -46,26 +42,26 @@ describe('CNH', () => {
       expect(typeof calcDv).toBe('string');
     });
 
-
     test.each(['50195471', '5831641745', ''])(
       'deve lançar erro de dv',
       (item) => {
         expect(() => CNH.checksum(item)).toThrow();
       }
     );
-
   });
 
   describe('mask', () => {
     test.each([
-      { value: '50195471143', expected: '501954711-43' },
-      { value: '58316474534', expected: '583164745-34' },
-      { value: '69044471146', expected: '690444711-46' },
+      { value: '14490435923', expected: '144904359-23' },
+      { value: '97286888262', expected: '972868882-62' },
+      { value: '621049358-60', expected: '621049358-60' },
+      { value: '491872350-50', expected: '491872350-50' },
     ])('máscara deve ser gerada corretamente', (input) => {
       const cnh = new CNH(input.value);
 
       expect(cnh.mask()).toBe(input.expected);
       expect(cnh.mask()).toHaveLength(12);
+      expect(cnh.mask()).toMatch(/^\d{9}-\d{2}$/)
     });
   });
 });
