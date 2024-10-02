354759a92fcb99d21efefcf504741189f1d4db83
Claudio Medeiros <klawdyo@gmail.com>
2024-10-02 11:57:01 -0300

    fix: remove applyMkas de dentro de Mask

---
+++ src/_helpers/mask.spec.ts
@@ -0,0 +1,17 @@
+import { Mask } from './mask';
+
+describe('applyMask()', () => {
+  const items = [
+    { value: '123456', mask: '00000-0', expected: '12345-6' },
+    { value: '12345', mask: '00000-0', expected: '01234-5' },
+    { value: '123456789', mask: '00000-0', expected: '12345-6' },
+    { value: '123456789', mask: '00000-0', expected: '12345-6' },
+  ];
+
+  test('Máscara deve ser aplicada com o valor e tamanho correto', () => {
+    items.forEach((item) => {
+      expect(item.expected.length).toBe(item.mask.length);
+      expect(new Mask(item.value).apply(item.mask)).toBe(item.expected);
+    });
+  });
+});
--- src/_helpers/mask.ts
+++ src/_helpers/mask.ts
@@ -1,9 +1,35 @@
-import { applyMask } from "../utils";
+import { clearValue, insertAtPosition } from '../utils';
 
 export class Mask {
-  constructor(private _value: string) { }
+  constructor(private _value: string) {}
 
+  /**
+   * applyMask()
+   * Aplica uma máscara a uma string
+   *
+   * @example
+   * applyMask('59650000', '00.000-000') // -> 59.650-000
+   * applyMask('99877665544', '(00) 0 0000-0000') // -> (99) 8 7766-5544
+   *
+   * @param {String|Number} value Valor original
+   * @param {String} mask
+   * @returns {String}
+   *
+   */
   apply(mask: string): string {
-    return applyMask(this._value, mask)
+    const maskLen = clearValue(mask).length;
+    let masked = clearValue(this._value, maskLen, {
+      fillZerosAtLeft: true,
+      trimAtRight: true,
+    });
+    const specialChars = ['/', '-', '.', '(', ')', ' '];
+
+    for (let position = 0; position < mask.length; position += 1) {
+      const current = mask[position];
+      if (specialChars.includes(current))
+        masked = insertAtPosition(masked, current, position);
+    }
+
+    return masked;
   }
 }
