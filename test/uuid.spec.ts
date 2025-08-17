ed109cec945edbea793a4dc7fe2bc00e31d75add
Cláudio Medeiros <klawdyo@gmail.com>
2025-08-17 11:58:29 -0300

    test: valida toString em uuid

--- test/uuid.spec.ts
+++ test/uuid.spec.ts
@@ -13,25 +13,25 @@ describe('UUID', () => {
     });
 
     test('Deve definir um uuid somente sem o "4" do v4', () => {
-      expect(()=>new UUID('00000000-0000-0000-0000-000000000000')).toThrow()
+      expect(() => new UUID('00000000-0000-0000-0000-000000000000')).toThrow()
     });
 
     test('Deve definir um uuid com letras não hexadecimais', () => {
-      expect(()=>new UUID('00000000-0000-0000-0000-0000X0000000')).toThrow()
+      expect(() => new UUID('00000000-0000-0000-0000-0000X0000000')).toThrow()
     });
 
     test('Deve definir um uuid com grupos com menos caracteres', () => {
-      expect(()=>new UUID('00000000-0000-0000-0000-00000000000')).toThrow()
+      expect(() => new UUID('00000000-0000-0000-0000-00000000000')).toThrow()
     });
 
     test('Deve definir um uuid com grupos com mais caracteres', () => {
-      expect(()=>new UUID('00000000-0000-0000-0000-000000000000A')).toThrow()
+      expect(() => new UUID('00000000-0000-0000-0000-000000000000A')).toThrow()
     });
   });
 
   describe('fake', () => {
     test('Deve definir um uuid fake', () => {
-      const uuid = UUID.fake();     
+      const uuid = UUID.fake();
       expect(new UUID(uuid.toString())).toBeDefined()
       expect(new UUID(uuid.value)).toBeDefined()
     });
@@ -40,14 +40,26 @@ describe('UUID', () => {
   describe('mask', () => {
     test('Deve definir a máscara de um uuid sem traços', () => {
       const uuid = new UUID('00000000000040000000000000000000');
-      console.log('Mask()', uuid.mask());
+
       expect(uuid.mask()).toBe('00000000-0000-4000-0000-000000000000')
     });
 
     test('Deve definir a máscara de um uuid com traços', () => {
       const uuid = new UUID('00000000-0000-4000-0000-000000000000');
-      console.log('Mask()', uuid.mask());
+
       expect(uuid.mask()).toBe('00000000-0000-4000-0000-000000000000')
+    });
+  });
+
+  describe('toString', () => {
+    test('Deve exibir o valor sem o caracteres especiais', () => {
+      const uuid = new UUID('00000000-0000-4000-0000-000000000000');
+
+      expect(uuid.toString()).toBe('00000000000040000000000000000000')
+      expect(uuid.toString()).toHaveLength(32)
+
+      expect(uuid.value).toBe('00000000000040000000000000000000')
+      expect(uuid.value).toHaveLength(32)
     });
   });
 });
