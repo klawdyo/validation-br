a58cdefc8a52adbd853d9c9eefad86f53e4ba4be
Claudio Medeiros <klawdyo@gmail.com>
2026-01-10 13:13:25 -0300

    release: 1.6.4 - conserta os scripts de publicação

--- publish.sh
+++ publish.sh
@@ -2,25 +2,30 @@
 
 echo 'Inicia execução do publish'
 
+rm    -r ./dist
+mkdir -p ./dist
+
+
 # Remove o dist originalmente para não deixar lixo
-echo 'Remove o ./dist para não deixar lixo'
-rm dist -r
+# echo 'Remove o ./dist para não deixar lixo'
+# rm dist -r
 
 # Executa o build do typescript
-echo 'Executa o build do typescript'
+# echo 'Executa o build do typescript'
 tsc
-
 
 
 # Remove os testes copiadps pelo build
 # rm -r dist/test
 
 # Move o conteúdo do dist/src para dentro de dist
-mv dist/src/* dist
+# cp package*.json ./dist/
+# cp readme.md ./dist/
+cp -r output/src/* ./dist/
 
 # Remove o dist/src
-rm dist/src -r
-rm dist/test -r
+# rm dist/src -r
+# rm dist/test -r
 
 # Copia package.json, package-lock.json, readme.md para dentro de dist
 # cp package*.json readme.md dist
