1a14ba2ea168c69c0bf848dc461b07d54e266488
andrevabo <andre@spacebits.com.br>
2026-04-02 15:37:31 -0300

    build: adiciona suporte a ESM para compatibilidade com Angular e bundlers modernos

--- publish.sh
+++ publish.sh
@@ -4,6 +4,7 @@ echo 'Inicia execução do publish'
 
 rm    -r ./dist
 mkdir -p ./dist
+mkdir -p ./dist/esm
 
 
 # Remove o dist originalmente para não deixar lixo
@@ -12,7 +13,11 @@ mkdir -p ./dist
 
 # Executa o build do typescript
 # echo 'Executa o build do typescript'
-tsc
+# Executa o build do typescript (CJS)
+./node_modules/.bin/tsc
+
+# Executa o build do typescript (ESM)
+./node_modules/.bin/tsc -p tsconfig.esm.json
 
 
 # Remove os testes copiadps pelo build
@@ -21,7 +26,14 @@ tsc
 # Move o conteúdo do dist/src para dentro de dist
 # cp package*.json ./dist/
 # cp readme.md ./dist/
+# Move o conteúdo do dist/src para dentro de dist (CJS)
 cp -r output/src/* ./dist/
+
+# Move o conteúdo do output-esm/src para dentro de dist/esm (ESM)
+cp -r output-esm/src/* ./dist/esm/
+
+# Marca o diretório esm como ESM para Node.js
+echo '{"type":"module"}' > ./dist/esm/package.json
 
 # Remove o dist/src
 # rm dist/src -r
